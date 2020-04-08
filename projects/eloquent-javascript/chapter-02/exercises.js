ae8de900ed80d79f66c5f32f0c8c7ff0f4c07a5f
Ubuntu <ubuntu@ip-172-31-87-184.ec2.internal>
2020-04-08 18:41:13 +0000

    adding matchy pseudocode

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -2,25 +2,98 @@
 ////////////////////////////////////////////////////////////////////////////////
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+/* For this exercise we are using a for loop to print a # sign to the console. The trick is that 
+* each pass of the loop must print one more # sign than the one before it. In order to do this, we
+* have to have something inside the function to increment the string as the loop increments. So I
+* will create a variable for the # signs to be stored in. Then inside the looper I will 
+* perform an operation that adds a # to the empty string value of the variable and then assigns the 
+* added # to the variable. Then the variable is console logged.
+*/
 
-function triangles() {
-
+function triangles(number) {
+  var string = '';
+    for(let i = 1; i <= number; i++){
+      string += '#'
+      console.log(string);
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function fizzBuzz() {
-  
+/*For this exercise I have to create a looper whereby the values between the given start and end
+* arguments can be stored in i sequentially. For each value of i, a conditional statement will run
+* which evaluates if a number is divisible by 3 and 5, divisible by three, or divisible by 5. There is an else statement
+* that prints the number i if none of the other conditions are true. Note: it is important that
+* the first conditional evaluates if a number if BOTH divisible by 3 and 5 because otherwise if we evaluated
+* divisible by 3 first, the code would stop running there and never print the correct value for the numbers which
+* are divisible by both.
+*/
+function fizzBuzz(start, end) {
+  for(let i = start; i <= end; i++){
+    if(i%3 === 0 && i%5 === 0){
+      console.log('fizzbuzz');
+    }else if(i%3 === 0){
+      console.log('fizz');
+    }else if(i%5 === 0){
+      console.log('buzz');
+    }else{
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+/*This should share some similarities with the triangle function. I'm going to write a 
+*/
 
-function drawChessboard() {
 
+
+
+
+
+function drawChessboard(number) {
+  let chessArrayOuter = [];
+  for(let i = 0; i < number; i++){
+    if(i%2 === 0){
+      let chessArrayInner = [];
+      for(let j = 0; j < number; j++ ){
+        
+        
+        if(j%2 === 0){
+          chessArrayInner.push(' ');
+        }else{
+          chessArrayInner.push('#');
+        }
+        
+      }
+        let chessString = chessArrayInner.join('');
+        chessArrayOuter.push(chessString);
+        
+        
+    }else if(i%2 !== 0){
+      let chessArrayInner = [];
+      for(let j = 0; j < number; j++){
+        
+        if(j%2 === 0){
+          
+          chessArrayInner.push('#');
+        }else{
+          chessArrayInner.push(' ');
+          
+        }
+      }let chessString = chessArrayInner.join('');
+        chessArrayOuter.push(chessString);
+        
+    }
+  }
+  
+  var chessStringOuter = chessArrayOuter.join('\n')
+  chessStringOuter = chessStringOuter + '\n'
+  console.log(chessStringOuter);
 }
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
--- projects/eloquent-javascript/chapter-03/exercises.js
+++ projects/eloquent-javascript/chapter-03/exercises.js
@@ -1,33 +1,82 @@
 ////////////////////////////////////////////////////////////////////////////////
 // min /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function min() {
-
+/*For this function a simple conditional chain will do. We take two inputs, compare their values and return the lower one.
+* It is also important to return one of the arguments if they are the same number.
+*/
+function min(num1, num2) {
+  if(num1 < num2){
+    return num1;
+  }else if(num2 < num1){
+    return num2;
+  }else{
+    return num1;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // isEven //////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function isEven() {
-
-}
+/* Here we are supposed to write a function that takes one parameter and returns 
+* true if the number = 0 or false if the number = 1. We are supposed to use recursion
+* which is modeled off a certain definition of odd/even. 0 is even, and n-2 is even. So
+* basically we just have to show that eventually, whichever number we are given will either
+* become 0 or 1 after enough recursion.
+*
+* Setting up the recursion requires an if chain. If the number is equal to 0, return true, 
+* if number = 1, return false, if neither case is true, we set up recursion by calling the function with 
+* an argument of number - 2. Each time the if and if else conditions fail we decrement the 
+* value of number by 2.
+*
+* Since feeding this function a negative number would result in an infinite recursion, we convert the
+* value of the argument with Math.abs() before the conditional chain
+*/
+function isEven(number) {
+  number = Math.abs(number);
+  if(number === 0){
+    return true;
+  }else if(number === 1){
+    return false;
+  }else return(isEven(number -2));
+  
+    
+}console.log(isEven(2))
 
 ////////////////////////////////////////////////////////////////////////////////
 // countChars //////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function countChars() {
-
+/* Here we are writing a function that takes two inputs, a string and a character.
+* It will output the number of times that the character occurs in the string.
+* In order to do this we define a variable count, which will be incremented every time we find 
+* the letter in the string. We create a for loop that searches the indexes of the
+* string. Inside the for loop is a sonditional. If the character at the string
+* index[i] is strictly equal to the argument given for char, we increment count. 
+* In the end we return count.
+*/
+function countChars(string, char) {
+  var count = 0;
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === char){
+      count +=1;
+    }
+  }return count;
+  
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // countBs /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function countBs() {
-
+/* This function is essentially the same as the above function except that it takes one 
+* less parameter. We hard code it to search the string to find instances where
+* string[i] is strictly equal to 'B'
+*/
+function countBs(string) {
+  var count = 0;
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === 'B'){
+      count+=1;
+    }
+  }return count;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
